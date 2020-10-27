ad3ed2cbdcca86a502572d2c3204ec0af2341442
Sarowal-1734 <sarowalhossain1997@gmail.com>
2020-10-27 12:51:33 +0600

    Data Added to the Database 1.0

--- app/src/main/java/com/dynamic_host/pets/CatalogActivity.java
+++ app/src/main/java/com/dynamic_host/pets/CatalogActivity.java
@@ -2,10 +2,12 @@ package com.dynamic_host.pets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +18,8 @@ import com.dynamic_host.pets.data.PetDbHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CatalogActivity extends AppCompatActivity {
+
+    private PetDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +35,18 @@ public class CatalogActivity extends AppCompatActivity {
             }
         });
 
+        mDbHelper = new PetDbHelper(this);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
         displayDatabaseInfo();
     }
 
     private void displayDatabaseInfo(){
         //to access database, initiate our subclass of SQLiteOpenHelper and pass the context of current activity
-        PetDbHelper mDbHelper = new PetDbHelper(this);
+        //PetDbHelper mDbHelper = new PetDbHelper(this);
         //Creating Database
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
         //To get a Cursor that contains all rows from the pets table
@@ -49,6 +59,18 @@ public class CatalogActivity extends AppCompatActivity {
         }
     }
 
+
+    private void insertPet(){
+        //Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(PetEntry.COLUMN_PET_NAME, "Dog");
+        values.put(PetEntry.COLUMN_PET_BREED, "Terrier");
+        values.put(PetEntry.COLUMN_PET_GENDER, PetEntry.GENDER_MALE);
+        values.put(PetEntry.COLUMN_PET_WEIGHT, 7);
+
+        db.insert(PetEntry.TABLE_NAME, null, values);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -63,6 +85,8 @@ public class CatalogActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
+                insertPet();
+                displayDatabaseInfo();
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
